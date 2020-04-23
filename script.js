98bca8b850288329092d6d80963b18ae342c4954
Charles Ivia <iviacharles@gmail.com>
2020-04-23 10:41:12 +0300

    Created logic to get winner and get results

--- script.js
+++ script.js
@@ -42,3 +42,22 @@ const players = (() => {
     return {player1, player2, markers}
     
 })();
+
+const game = () => {
+    const totalMoves = [];
+    let turn = players.player1.isFirstMove === true ? players.player1 : players.player2;
+    domEl.gameInfo.innerHTML = `${turn.name} goes first`;
+
+    const isWinner = (playerArr) => {
+        const winArr = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6]];
+        return winArr.some(winVal => winVal.every(winPos => playerArr.includes(winPos)));
+    }
+
+    const getResult = (player, square) => {
+        const position = gameBoard.squares.indexOf(square);
+        player.positions.push(position);
+        if(isWinner(player.positions)) return `${player.name} is the winner!`;
+        if(totalMoves.length >= 9) return "We have a Draw!";
+        return "no result"
+    }
+}
